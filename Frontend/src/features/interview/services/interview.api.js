f7afc3d18b786c02a07b3276a3647d587e6d2d8d
Souvik Biswas <capitalcoderzz@gmail.com>
2026-07-06 00:03:58 +0530

    Send Bearer token on interview API requests

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -5,6 +5,12 @@ const api = axios.create({
     withCredentials: true,
 })
 
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token")
+    if (token) config.headers.Authorization = `Bearer ${token}`
+    return config
+})
+
 
 /**
  * @description Service to generate interview report based on user self description, resume and job description.
